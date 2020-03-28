8f06529e9a7c744b1155ea0b8a876868abee6b04
Mat Cabral <mathewcabral@gmail.com>
2020-03-28 03:14:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+def 
+  oxford_comma(array)
+if array.count == 1
+ return array
+elsif array.count == 2
+  return  array.join(" and ")
+else
+  array[-1] = "and #{array[-1]}"
+  return array.join(", ")
+end
 
 end
